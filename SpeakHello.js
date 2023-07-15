f028fd1fa031cdc1b6ed8414d28db5dd708f1fcb
Hana11-tech <136694823+Hana11-tech@users.noreply.github.com>
2023-07-15 12:31:42 +0530

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,19 @@
+
+(
+  function(window){
+    var helloSpeaker = {};
+    helloSpeaker.name=name;
+    var speakWord = "Hello ";
+    helloSpeaker.speak = function ()
+     {
+      console.log(speakWord + " " + name);
+    }
+
+
+    window.helloSpeaker = helloSpeaker;}
+)(window);
+
+
+
+
+
